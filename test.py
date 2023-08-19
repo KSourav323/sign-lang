8fb9f52b5ef22cb3c84f758ef2f50573874527b0
KSourav323 <84306780+KSourav323@users.noreply.github.com>
2023-08-19 09:05:48 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,70 @@
+import cv2
+import numpy as np
+import math
+import time
+
+from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=1)
+classifier = Classifier("Model/keras_model.h5", "Model/labels.txt")
+offset = 20
+imgSize = 300
+counter = 0
+labels = ["Good Luck", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+folder = "Data"
+
+while True:
+    success, img = cap.read()
+    imgOutput = img.copy()
+    hands ,img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+        
+        try:
+            imgWhite = np.ones((imgSize, imgSize, 3),np.uint8)*255
+            imgCrop = img[y-offset:y+h+offset, x-offset:x+w+offset]
+            imgCropShape = imgCrop.shape
+            aspectRatio = h/w
+            
+            if aspectRatio > 1:
+                k=imgSize/h
+                wCal = math.ceil(k*w)
+                imgResize = cv2.resize(imgCrop, (wCal, imgSize))
+                imgResizeShape = imgResize.shape
+                wGap = math.ceil((imgSize-wCal)/2)
+                imgWhite[:,wGap:wCal+wGap] = imgResize
+
+                prediction, index = classifier.getPrediction(imgWhite, draw= False)
+                print(prediction, index)
+            
+            else:
+                k=imgSize/w
+                hCal = math.ceil(k*h)
+                imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+                imgResizeShape = imgResize.shape
+                hGap = math.ceil((imgSize-hCal)/2)
+                imgWhite[hGap: hCal+hGap,:] = imgResize
+
+                prediction, index = classifier.getPrediction(imgWhite, draw = False)
+            
+
+            cv2.putText(imgOutput, labels[index],(x-20,y-50),cv2.FONT_HERSHEY_COMPLEX, 2, (82,235,52),2)
+            cv2.rectangle(imgOutput,(x-offset,y-offset),(x+w+offset, y+h+offset), (52,235,143), 4)
+            
+            cv2.imshow("ImageCrop", imgCrop)
+            cv2.imshow("ImageWhite", imgWhite)
+        except:
+            print("")
+        
+ 
+    cv2.imshow("SILICON VALLEY PROJECT",imgOutput)
+    
+    key = cv2.waitKey(1)
+
+    if key == ord("s"):
+        counter += 1
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg',imgWhite)
+        print(counter)
